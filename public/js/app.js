5e2d619f52c88149eb97f7ce3b3435f369613990
deal-1997 <70301678+deal-1997@users.noreply.github.com>
2020-11-30 04:14:22 +0000

    adding index files

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app     = express();
+const fetch   = require("node-fetch");
+const pool    = require("./dbPool.js");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+//routes
+app.get("/", async function(req, res){
+   
+      app.get("/search", async function(req, res){
+       
+       let keyword = "";
+       if (req.query.keyword) {
+           keyword = req.query.keyword;
+       }
+  
+       let apiUrl = 'ttps://images.unsplash.com/flagged/photo-1556047124-cd8579bd09b3?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjEwMzUxOH0';
+       let response = await fetch(apiUrl);
+       let data = await response.json();
+       
+       let imageUrlArray = [];
+       for (let i = 0; i < data.length; i++) {
+           imageUrlArray.push(data[i].urls.small);
+       }
+    
+       res.render("results", {"imageUrl": data[0].urls.small, "imageUrlArray":imageUrlArray});
+       
+       
+      });
+      
+       app.get("/getKeywords",  function(req, res) {
+       let sql = "SELECT DISTINCT keyword FROM favorites ORDER BY keyword";
+       let imageUrl = ["img/favorite.png"];
+       pool.query(sql, function (err, rows, fields) {
+       if (err) throw err;
+       console.log(rows);
+       res.render("favorites", {"imageUrl": imageUrl, "rows":rows});
+  });  
+});//getKeywords
+
+       
+});
---
+++ public/js/functions.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    
+    $(document).on("click", ".favoriteIcon", function() {
+        var imageURL = $(this).prev().attr("src");
+      if ($(this).attr("src") == "img/favoriteoff.png") {
+        $(this).attr("src", "img/favoriteon.png");
+        updateFavorite("add", imageURL);//inserts a new record
+      }
+      else {
+        $(this).attr("src", "img/favoriteoff.png");
+        updateFavorite("delete", imageURL);//delete a record
+      }
+    });
+    
+    $(".keywordLink").on("click", function() {
+        
+        $.ajax({
+            method: "get",
+            url: "/api/displayFavorites",
+            data: {
+                    "keyword": $(this).text().trim(),
+                },
+            success: function(rows, status) {
+                    $("#favorites").html("");
+                    rows.forEach(function(row, i){
+                        $("#favorites").append(
+                        "<img class='image' src='" + row.imageURL + 
+                        "' width='200' height='200'>");
+                        $("#favorites").append("<img class='favoriteIcon' src='img/favoriteon.png'" +
+                        "width='20'>");
+                        if (i % 4 == 3) {
+                            $("#favorites").append("<br>");
+                        }
+                    });
+            }
+        });//ajax
+    });
+
+    function updateFavorite(action, imageURL) {
+        $.ajax({
+            method: "get",
+            url: "/api/updateFavorites",
+            data: {"imageURL": imageURL,
+                    "keyword": $("#keyword").val(),
+                    "action": action
+            }
+        });//ajax
+    }
+});
