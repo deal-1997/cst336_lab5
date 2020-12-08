6b1ebc0aa19a63a6bd69c093550a1022afd3db2e
deal-1997 <70301678+deal-1997@users.noreply.github.com>
2020-12-08 23:42:38 +0000

    adding changes to functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     let imageUrl= $(this).prev().attr("src");
 
         if ($(this).attr("src") == "img/favorite.png"){
-            $(this).attr("src", "img/favorite_on.png");
+            $(this).attr("src", "img/favoriteon.png");
             updateFavorite("add", imageUrl, keyword);
         }
         else{
@@ -44,7 +44,7 @@ async function updateFavorite(action, imageUrl, keyword) {
                  let htmlString = "";
                  data.forEach(function(row){
                     htmlString += "<img class='image' src='"+row.imageURL+"' width='200' height='200'>";
-                    htmlString += "<img class='favoriteIcon' src='img/favorite_on.png' width='20'>";
+                    htmlString += "<img class='favoriteIcon' src='img/favoriteon.png' width='20'>";
                     });
                       
                $("#favorites").append(htmlString);
@@ -57,9 +57,8 @@ async function updateFavorite(action, imageUrl, keyword) {
     $("#favorites").on("click", ".favoriteIcon", function(){
                 
       let favorite = $(this).prev().attr("src");
-            
       if ($(this).attr("src") == 'img/favorite.png') {
-                $(this).attr("src","img/favorite_on.png");
+                $(this).attr("src","img/favoriteon.png");
          updateFavorite("add",favorite, $("#keywordSelected").val());
             } else {
                 $(this).attr("src","img/favorite.png");
